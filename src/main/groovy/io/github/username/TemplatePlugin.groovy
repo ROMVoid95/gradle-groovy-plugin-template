452d54181079f0682f271e4d5ff9ce046f3b4895
ROM <rom.void95@gmail.com>
2023-03-11 06:31:07 -0600

    init

---
+++ src/main/groovy/io/github/username/TemplatePlugin.groovy
@@ -0,0 +1,12 @@
+package io.github.username
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class MavenBasePublishPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
